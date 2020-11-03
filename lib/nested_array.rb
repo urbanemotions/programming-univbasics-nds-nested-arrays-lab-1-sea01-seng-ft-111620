0e6c65b1e4fddb6cb3644e60158e8bc437147a16
Sin Wildy <sw.wildy@gmail.com>
2020-11-03 22:27:35 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,8 +36,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  assembled_matrix[1][1]
-  sorted_matrix[1][1]
+  matrix[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
